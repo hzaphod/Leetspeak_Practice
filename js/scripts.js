7c176fb7531b1c08c4e0486f3477d19d215178d5
Heather McCarthy <newwavehealing@gmail.com>
2016-03-02 20:27:19 -0800

    updates to html to match example and to scripts.js to basically mimic the example via copy and paste

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,39 @@
+
 var leetspeak = function(word) {
 
 var leetWord="";
 
     for (var i = 0; word.length; i++){
-      if ((word.charAt(i).toLowerCase() === "e"){
-        leetWord = leetWord.concat("3");
-      }else {
-        leetWord = leetWord.concat(word.charAt(i));
-      }
-    }
-};
+        if ((word.charAt(i).toLowerCase() === "s") && (i!=0)){
+          leetWord = leetWord.concat("z");
+        }else if (word.charAt(i).toLowerCase() === "o"){
+          leetWord = leetWord.concat("0");
+        }else if (word.charAt(i) === "I"){
+          leetWord = leetWord.concat("1");
+        }else if (word.charAt(i).toLowerCase() === "e"){
+          leetWord = leetWord.concat("3");
+        }else {
+          leetWord = leetWord.concat(word.charAt(i));
+        }
+      }  // end of for loop
+
+
+      return leetWord;
+
+}; //end of function leetspeak
+
+$(document).ready(function() {
+  $("form#leetspeak").submit(function(event) {
+    var word = $("input#words").val();
+
+    var result = leetspeak(word);
+
+    $("#demo").text(result);
+
+    $("#result").show();
+    $("button.btn").click(function(){
+      location.reload();
+    })
+    event.preventDefault();
+  });
+});
